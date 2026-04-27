e06942d06dcb273e5f8f26c19bda119aedeb23f0
Azukinako253 <alien_1213@icloud.com>
2026-04-27 16:51:55 +0900

    first_commit:exercise427.js

---
+++ exercise427.js
@@ -0,0 +1,15 @@
+const cafe = {
+  name: "Progateカフェ",
+  businessHours:{
+    opening:"10:00(AM)",
+    closing:"8:00(PM)"
+  }
+};
+console.log(`店名:${cafe.name}`);
+console.log(`営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+
+function toMinutes(hours){
+  const minutes=hours*60;
+  return minutes;
+};
+console.log(`勤務時間は${toMinutes(7)}から${toMinutes(8)}分です。`);
